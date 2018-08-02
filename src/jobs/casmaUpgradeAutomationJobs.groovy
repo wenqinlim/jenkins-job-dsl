e1b607494a93a8ffb8aab1e28010dbce0ffa6dfd
wenqin.1 <wenqin1@wenqin.1>
2018-08-02 09:51:27 +0800

    Sample Job for CAS automation

---
+++ src/jobs/casmaUpgradeAutomationJobs.groovy
@@ -0,0 +1,41 @@
+String basePath = 'example6'
+
+folder(basePath) {
+    description 'This example shows how to include script resources from the workspace for CASMA upgrade automation.'
+}
+
+job("$basePath/run-automation-job") {
+
+    parameters {
+        stringParam('RELEASE_VERSION', '2.3.5.1', 'Build version under test')
+        stringParam('UPGRADE_PATH', '1.3.7.1::2.1.1.1')
+        stringParam('DEPLOY_CONFIG, 'kl.cas-s400-a4-03')
+        stringParam('TESTS_HOST_IP', '10.199.110.73')
+        stringParam('TESTS_MAG2_TESTMODE', 'casma')
+        stringParam('TESTS_HOST_TYPE', 's400')
+        stringParam('JENKINS_HTML_REPORT', 'disabled')
+        stringParam('netmask', '255.255.255.0')
+        stringParam('gateway', '10.199.110.253')
+        stringParam('dns', '8.8.8.8')
+        stringParam('serial_switch', '10.199.97.23')
+        stringParam('serial_port', '9')
+        stringParam('serial_ssh_base_port', '5100')
+        stringParam('serial_user', 'administrator')
+        stringparam('serial_password', 'password')
+        stringParam('bmc_ip', '10.199.112.73')
+        stringParam('bmc_user', 'admin')
+        stringParam('bmc_password', 'password')
+
+    }
+    steps {
+        shell readFileFromWorkspace('src/scripts/runTest.sh')
+    }
+
+    publishers {
+        #groovyPostBuild readFileFromWorkspace('src/scripts/postProcess.groovy')
+        junit {
+            pattern('project-${DEPLOY_CONFIG}/tests/Reports/*.xml')
+        }
+    }
+}
+
---
+++ src/scripts/runTest.sh
@@ -0,0 +1,49 @@
+sudo -s chmod -R 777 /home/wenqin1/Jenkins-Work-Directory/project-${DEPLOY_CONFIG} || echo "Jenkins work directory is clean"
+rm -R /home/wenqin1/Jenkins-Work-Directory/project-${DEPLOY_CONFIG} || echo "Jenkins work directory is clean"
+mkdir -p /home/wenqin1/Jenkins-Work-Directory/project-${DEPLOY_CONFIG} || echo "Couldn't create work directory for project"
+cp -R /home/wenqin1/gitrepo/* /home/wenqin1/Jenkins-Work-Directory/project-${DEPLOY_CONFIG}/
+cd /home/wenqin1/Jenkins-Work-Directory/project-${DEPLOY_CONFIG}/tests
+
+cat > jenkins-test-env.sh <<EOF
+export MAG2_RAPI=${TESTS_HOST_IP}
+export MAG2HOST=${TESTS_HOST_IP}
+export MAG2_ADMINUSER=admin
+export MAG2_ADMINPASS=admin123
+export MAG2_TESTMODE=${TESTS_MAG2_TESTMODE}
+export SKIP_SETUP_BOX=yes
+export HTML_REPORT=${JENKINS_HTML_REPORT}
+export MAG2_HOST_TYPE=${TESTS_HOST_TYPE}
+export RELEASE_VERSION=${RELEASE_VERSION}
+export UPGRADE_PATH=${UPGRADE_PATH}
+
+export netmask=${netmask}
+export gateway=${gateway}
+export dns1=${dns1}
+export serial_switch=${serial_switch}
+export serial_port=${serial_port}
+export serial_ssh_base_port=${serial_ssh_base_port}
+export serial_user=${serial_user}
+export serial_password=${serial_password}
+export bmc_ip=${bmc_ip}
+export bmc_user=${bmc_user}
+export bmc_password=${bmc_password}
+EOF
+chmod +x jenkins-test-env.sh
+
+docker kill project-${DEPLOY_CONFIG} || echo "Couldn't Kill the container"
+docker rm project-${DEPLOY_CONFIG} || echo "Could not remove the container"
+docker build --rm=true -t "bluecoat/project-${DEPLOY_CONFIG}" ./.jenkins
+docker run --rm \
+  --name "project-${DEPLOY_CONFIG}" \
+  -v "$(pwd)/output":"/build/output" \
+  -v "$(pwd)":"/git/tests" \
+  -v "/var/lib/jenkins:/var/lib/jenkins":ro \
+  -e "JENKINS_USER=$(id -un)" \
+  -e "JENKINS_UID=$(id -u)" \
+  -e "JENKINS_GROUP=$(id -gn)" \
+  -e "JENKINS_GID=$(id -g)"\
+  -e "RELEASE_VERSION=${RELEASE_VERSION}" \
+  -e "UPGRADE_PATH=${UPGRADE_PATH}" \
+  -e "DEPLOY_CONFIG=${DEPLOY_CONFIG}" \
+  "bluecoat/project-${DEPLOY_CONFIG}" \
+  /git/tests/.jenkins/run.sh
