02dc38580b8048d0bbd7c5549fb4dd7ba6c29791
wenqin.1 <wenqin1@wenqin.1>
2018-08-02 11:09:53 +0800

    Run multi config jobs via file parameterization

--- src/jobs/casmaUpgradeAutomationJobs.groovy
+++ src/jobs/casmaUpgradeAutomationJobs.groovy
@@ -4,40 +4,30 @@ folder(basePath) {
     description 'This example shows how to include script resources from the workspace for CASMA upgrade automation.'
 }
 
-job("$basePath/run-automation-job") {
+[
+    [uPath: 'direct'], 
+    [uPath: 'incremental']
+].each { Map config ->
 
-    parameters {
-        stringParam('RELEASE_VERSION', '2.3.5.1', 'Build version under test')
-        stringParam('UPGRADE_PATH', '1.3.7.1::2.1.1.1')
-        stringParam('DEPLOY_CONFIG', 'kl.cas-s400-a4-03')
-        stringParam('TESTS_HOST_IP', '10.199.110.73')
-        stringParam('TESTS_MAG2_TESTMODE', 'casma')
-        stringParam('TESTS_HOST_TYPE', 's400')
-        stringParam('JENKINS_HTML_REPORT', 'disabled')
-        stringParam('netmask', '255.255.255.0')
-        stringParam('gateway', '10.199.110.253')
-        stringParam('dns', '8.8.8.8')
-        stringParam('serial_switch', '10.199.97.23')
-        stringParam('serial_port', '9')
-        stringParam('serial_ssh_base_port', '5100')
-        stringParam('serial_user', 'administrator')
-        stringParam('serial_password', 'password')
-        stringParam('bmc_ip', '10.199.112.73')
-        stringParam('bmc_user', 'admin')
-        stringParam('bmc_password', 'password')
+    job("$basePath/upgrade-${config.uPath}") {
 
-    }
+        description "${config.uPath} upgrade job"
 
-    customWorkspace('/home/wenqin1/Jenkins-Work-Directory/')
+        parameters {
+            readFileFromWorkspace('src/scripts/uParameters_${config.uPath}')
+        }
 
-    steps {
-        shell readFileFromWorkspace('src/scripts/runTest.sh')
-    }
+        customWorkspace('/home/wenqin1/Jenkins-Work-Directory/')
 
-    publishers {
-        archiveJunit('project-${DEPLOY_CONFIG}/tests/Reports/*.xml') {
-           retainLongStdout()
-           healthScaleFactor(1.0)
+        steps {
+            shell readFileFromWorkspace('src/scripts/runTest.sh')
+        }
+
+        publishers {
+            archiveJunit('project-${DEPLOY_CONFIG}/tests/Reports/*.xml') {
+               retainLongStdout()
+               healthScaleFactor(1.0)
+            }
         }
     }
 }
