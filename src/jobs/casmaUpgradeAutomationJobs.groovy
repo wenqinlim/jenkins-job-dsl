cac44ed38ca27462248b994de50310a65a7727bb
wenqinlim <41657944+wenqinlim@users.noreply.github.com>
2019-04-22 11:03:21 +0800

    Latest release CA 2.4

--- src/jobs/casmaUpgradeAutomationJobs.groovy
+++ src/jobs/casmaUpgradeAutomationJobs.groovy
@@ -7,25 +7,25 @@ folder(basePath) {
 [
     [uPath: 'direct', 
      DEPLOY_CONFIG: 'kl.cas-s500-a1-01', 
-     UPGRADE_PATH: '1.3.7.1::2.1.1.1_debug::latest:casma_2_3_debug_bcsi', 
+     UPGRADE_PATH: '1.3.7.1::2.1.1.1_debug::2.3.5.1_debug::latest:casma_2_4_debug_bcsi', 
      SCHEDULE_TIME: 'H H(5-7) * * *', 
      PORT_FWD: '5908', 
      SAMPLE_REPO: 'http://10.199.97.67/sample/automation'], 
     [uPath: 'incremental', 
      DEPLOY_CONFIG: 'kl.cas-s400-a4-03', 
-     UPGRADE_PATH: '1.3.7.1::2.1.1.1_debug::2.2.1.1_debug::2.3.1.1_debug::2.3.1.2_debug::latest:casma_2_3_debug_bcsi', 
+     UPGRADE_PATH: '1.3.7.1::2.1.1.1_debug::2.2.1.1_debug::2.3.1.1_debug::2.3.1.2_debug::2.3.5.1_debug::latest:casma_2_4_debug_bcsi', 
      SCHEDULE_TIME: 'H H(5-7) * * *', 
      PORT_FWD: '5907', 
      SAMPLE_REPO: 'http://10.199.97.67/sample/automation'],
-    [uPath: '1.3.7.x-2.1.1-2.3.5', 
+    [uPath: '1.3.7.x-2.1.1-2.2.1-2.4.1', 
      DEPLOY_CONFIG: 'kl.cas-s500-a1-01', 
-     UPGRADE_PATH: '1.3.7.1::2.1.1.1_debug::latest:casma_2_3_debug_bcsi', 
+     UPGRADE_PATH: '1.3.7.1::2.1.1.1_debug::2.2.1.1_debug::latest:casma_2_4_debug_bcsi', 
      SCHEDULE_TIME: 'H H(1-3) * * *', 
      PORT_FWD: '5906', 
      SAMPLE_REPO: 'http://10.199.97.67/sample/automation'], 
-    [uPath: '1.3.7.x-2.1.1-2.2.1-2.3.5', 
+    [uPath: '1.3.7.x-2.1.1-2.3.1-2.4.1', 
      DEPLOY_CONFIG: 'kl.cas-s400-a4-03', 
-     UPGRADE_PATH: '1.3.7.1::2.1.1.1_debug::2.2.1.1_debug::latest:casma_2_3_debug_bcsi', 
+     UPGRADE_PATH: '1.3.7.1::2.1.1.1_debug::2.3.1.1_debug::latest:casma_2_4_debug_bcsi', 
      SCHEDULE_TIME: 'H H(1-3) * * *', 
      PORT_FWD: '5905', 
      SAMPLE_REPO: 'http://10.199.97.67/sample/automation']
@@ -37,7 +37,7 @@ folder(basePath) {
 
         parameters {
             stringParam('DEPLOY_CONFIG', "${config.DEPLOY_CONFIG}")
-            stringParam('RELEASE_VERSION', "2.3.5.1")
+            stringParam('RELEASE_VERSION', "2.4.1.1")
             stringParam('UPGRADE_PATH', "${config.UPGRADE_PATH}")
             stringParam('SCHEDULE_TIME', "${config.SCHEDULE_TIME}")
             stringParam('PORT_FWD', "${config.PORT_FWD}")
